f3cf9bfa8b91fb3044d95e130a03a3bc21ed03d8
PayalPatel03 <p6579113@gmail.com>
2025-07-24 16:15:00 +0530

    done

--- employee-manag-crud/index.js
+++ employee-manag-crud/index.js
@@ -1,32 +1,53 @@
+const express = require("express");
+const app = express();
+const port = 1503;
+let employees = [];
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
-const express=require('express');
-const app=express();
-const port=1503;
-let employees=[];
-app.set("view engine","ejs");
-app.use(express.urlencoded({extended:true}));
-
-app.get('/',(req,res)=>{
-    console.log(employees);
-    return res.render("index",{
-        employees
-    })
+app.get("/", (req, res) => {
+  console.log(employees);
+  return res.render("index", {
+    employees,
+  });
 });
 app.get("/employee/delete/:id", (req, res) => {
-    const {id}=req.params;
-    employees = employees.filter((employee) => employee.id != id);
-    return res.redirect("/");
+  const { id } = req.params;
+  employees = employees.filter((employee) => employee.id != id);
+  return res.redirect("/");
+});
+app.get("/employee/edit/", (req, res) => {
+  const { id } = req.query;
+  let data = employees.find((employee) => employee.id == id);
+  return res.render("edit", {
+    data,
+  });
+});
+app.post("/employee/edit/", (req, res) => {
+  const { id } = req.query;
+
+  employees = employees.map((employee) => {
+    if (employee.id == id) {
+      return {...employee,
+        employee_name: req.body.employee_name,
+        dep: req.body.dep,
+        emp_salary: req.body.emp_salary,
+      };
+    }
+    return employee;
+  });
+
+  return res.redirect("/");
 });
 
 app.post("/login", (req, res) => {
-  const uniqueId = Date.now(); 
-
+  const uniqueId = Date.now();
   let obj = {
-    id: uniqueId,                 
-    employee_id: uniqueId,        
+    id: uniqueId,
+    employee_id: uniqueId,
     employee_name: req.body.employee_name,
     dep: req.body.dep,
-    emp_salary: req.body.emp_salary
+    emp_salary: req.body.emp_salary,
   };
 
   employees.push(obj);
@@ -34,13 +55,11 @@ app.post("/login", (req, res) => {
   return res.redirect("/");
 });
 
-
-app.listen(port,(err)=>{
-    if(err){
-        console.log(err.message);
-    }
-    else{
-        console.log("Server started.");
-       console.log("http://localhost:" + port);
-    }
-})
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log("Server started.");
+    console.log("http://localhost:" + port);
+  }
+});
