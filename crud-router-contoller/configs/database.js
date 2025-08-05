aae562e4dc55c53d5cf851ffd286aadf872abe3e
PayalPatel03 <p6579113@gmail.com>
2025-08-05 16:48:10 +0530

    done

--- crud-router-contoller/configs/database.js
+++ crud-router-contoller/configs/database.js
@@ -1,5 +1,4 @@
 const { default: mongoose } = require("mongoose");
-
 const db=mongoose.connection;
 mongoose.connect('mongodb+srv://p6579113:12345@cluster0.zlavh0m.mongodb.net/product');
 db.on('connected',(err)=>{
