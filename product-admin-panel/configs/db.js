e46c59f97da589725e9b3b69ae4ea523a1970543
PayalPatel03 <p6579113@gmail.com>
2025-08-09 14:38:29 +0530

    done

--- product-admin-panel/configs/db.js
+++ product-admin-panel/configs/db.js
@@ -3,7 +3,7 @@ const { default: mongoose } = require("mongoose");
 
 const db = async ()=>{
     try {
-        await mongoose.connect('mongodb+srv://aksharparekh401:12345@cluster0.ncwztql.mongodb.net/product')
+        await mongoose.connect('mongodb+srv://p6579113:12345@cluster0.zlavh0m.mongodb.net/product')
         console.log("Database connected..")
     } catch (error) {
         console.log(error)
