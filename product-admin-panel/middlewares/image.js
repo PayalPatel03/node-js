8f281d61de83775af23fe421a47a6735392363a7
PayalPatel03 <p6579113@gmail.com>
2025-08-12 16:08:23 +0530

    done

--- product-admin-panel/middlewares/image.js
+++ product-admin-panel/middlewares/image.js
@@ -1,36 +1,14 @@
-// const multer = require("multer");
-
-// const storage=multer.diskStorage({
-//     destination:function(req,res,cb){
-//         cb(null,'/uploads');
-//     },
-//     filename:function(req,file,cb){
-//         cb(null,Date.now()+ "-"+ file.originalname)
-//     }
-// })
-// const image=multer({storage:storage}).single('image');
-
-// module.exports=image;
-
 const multer = require("multer");
-const path = require("path");
-const fs = require("fs");
 
-// Create uploads folder if not exists
-const uploadDir = path.join(__dirname, "../uploads");
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
-}
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, uploadDir); 
+const storage=multer.diskStorage({
+    destination:function(req,res,cb){
+        cb(null,'uploads');
     },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + "-" + file.originalname);
+    filename:function(req,file,cb){
+        cb(null,Date.now()+ "-"+ file.originalname)
     }
-});
+})
+const image=multer({storage:storage}).single('image');
 
-const image = multer({ storage: storage }).single('image');
+module.exports=image;
 
-module.exports = image;
